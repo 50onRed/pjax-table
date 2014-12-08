26c81f2b3af63c01e5f5928e6f4ed1a2d7755a11
Tim <timmil@50onred.com>
2014-12-08 11:56:02 -0500

    overrides all default search events

--- src/pjax_table.js
+++ src/pjax_table.js
@@ -219,15 +219,14 @@
           $el.trigger('search.table', { query: query });
           $.extend(queryState, { q: query, page: 1 });
           pjaxForContainer();
-          e.preventDefault(); //prevents the search input from firing default search event
         }
         // if esc clear search and load full table
         if (e.keyCode == 27) {
           $el.trigger('clear_search.table');
           $.extend(queryState, { q: '', page: 1 });
           pjaxForContainer();
-          e.preventDefault();
         }
+        return false; // stops the default search event from firing
       });
 
       // Search clear
