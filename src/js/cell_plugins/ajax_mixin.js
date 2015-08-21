13f53270f8ef653a7c3dc5f351aff2e466295d08
mparke <mparke@50onred.com>
2015-08-21 15:48:20 -0400

    add cell ajax post mixin

---
+++ src/js/cell_plugins/ajax_mixin.js
@@ -0,0 +1,35 @@
+'use strict';
+var CellPluginMixin = require('./cell_plugin_mixin');
+
+function AjaxMixin(el, options) {
+  CellPluginMixin.call(this, el, options);
+
+  this._url = options.url || this._$el.data('url');
+
+  this._save = function save(data) {
+    this._$el.trigger('plugin:before:save');
+
+    return $.ajax({
+      type: 'POST',
+      url: this._url,
+      dataType: 'json',
+      contentType: 'application/json',
+      data: JSON.stringify(data),
+      context: this
+    }).done(function (data, textStatus, jqXHR) {
+      if (data.status === 'success') {
+        this._$el.trigger('plugin:save:success', data);
+      } else {
+        this._$el.trigger('plugin:save:error', data);
+      }
+    }).fail(function (jqXHR, textStatus, errorThrown) {
+      this._$el.trigger('plugin:save:error', {
+        textStatus: textStatus,
+        errorThrown: errorThrown,
+        jqXHR: jqXHR
+      });
+    });
+  };
+}
+
+module.exports = AjaxMixin;
