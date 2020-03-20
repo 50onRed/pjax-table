5d6a8e145821de62e25469be2988db604de73cc9
Rob Harrigan <rharrigan@50onred.com>
2020-03-20 11:02:01 -0400

    Merge fix for selected rows

--- src/js/pjax_table.js
+++ src/js/pjax_table.js
@@ -338,8 +338,12 @@ $.extend(PjaxTable.prototype, {
     this._disableRowCheckboxChangeHandling();
 
     if ($checkbox.prop('checked')) {
-      this._$el.find('td[data-property=' + property + '] input[type="checkbox"]').prop('checked', true);
-      this._$tbody.find('tr').addClass('ui-selected');
+      var $checkboxes = this._$el.find('td[data-property=' + property + '] input[type="checkbox"]:enabled');
+      $checkboxes.each(function () {
+        var $rowCheckbox = $(this);
+        $rowCheckbox.prop('checked', true);
+        $rowCheckbox.closest('tr').addClass('ui-selected');
+      });
       this._$el.trigger('table:select:all');
     } else {
       this._$el.find('td[data-property=' + property + '] input[type="checkbox"]').prop('checked', false);
